ea379c0936963400ea034ee8f729992af8327e8e
nde95 <ndennison268@gmail.com>
2023-04-01 09:17:46 -0400

    grabbing user value

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var button = document.getElementById("enter");
 var shoppingList = document.getElementById("shoppinglist");
 var ul = document.querySelector("ul")
+var input = document.getElementById("userinput")
 
 button.addEventListener("click", function() {
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode("testing"));
+	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 })
