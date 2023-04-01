6c3685ad104497235fafb39f1d3726589d1968ca
nde95 <ndennison268@gmail.com>
2023-04-01 12:52:10 -0400

    final update
    added css and fomatted for use

--- script.js
+++ script.js
@@ -6,15 +6,29 @@ var container = document.getElementById("container")
 var listItems = document.querySelectorAll('li')
 
 
+let removeItem = function() {
+	this.parentNode.remove();
+}
+
+
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
+	// button
+	var removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "btn");
+    removeButton.appendChild(document.createTextNode("Remove"));
+    // list
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+	// functionality of adding toggle to new text
+	ul.appendChild(li).addEventListener("click", finishItem);
+	// adding button to text
+	ul>li.appendChild(removeButton).addEventListener("click", removeItem);
 }
 
 function addListAfterClick() {
@@ -32,11 +46,15 @@ function addListAfterKeypress (event) {
 // DONE ELEMENT
 
 for (var i = 0; i < listItems.length; i++) {
-	// console.log(listItems[i]);
-	listItems[i].addEventListener("click", function () {
-		event.target.classList.toggle("finished")
-	})
+	listItems[i].addEventListener("click", finishItem);
 }
+let finishItem = function() {
+	event.target.classList.toggle("finished")
+}
+
+// Remove button
+
+
 
 button.addEventListener("click", addListAfterClick)
 
