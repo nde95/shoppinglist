139555ee0bbdc15a014f74efd08ba7d0156990fa
nde95 <ndennison268@gmail.com>
2023-04-01 09:42:41 -0400

    refactored

--- script.js
+++ script.js
@@ -3,22 +3,30 @@ var shoppingList = document.getElementById("shoppinglist");
 var ul = document.querySelector("ul")
 var input = document.getElementById("userinput")
 
-button.addEventListener("click", function() {
-	if (input.value.length > 0) {
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
 	}
+}
 
-})
-
-input.addEventListener("keypress", function() {
-	if (input.value.length > 0 && event.which === 13) {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+function addListAfterKeypress (event) {
+	if (inputLength() > 0 && event.which === 13) {
+	createListElement();
 	}
+}
 
-})
+
+button.addEventListener("click", addListAfterClick)
+
+input.addEventListener("keypress", addListAfterKeypress)
